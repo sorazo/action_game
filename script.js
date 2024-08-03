218adfdcf7fc92e1de5342720e621eb3bfe01185
sorazo <picanarachu@outlook.com>
2024-08-04 00:14:40 +0900

    JavaScriptで戦闘ゲーム制作 #2

--- script.js
+++ script.js
@@ -23,7 +23,34 @@ insertText("enemyName", enemyData["name"]);
 insertText("currentEnemyHp", enemyData["hp"]);
 insertText("maxEnemyHp", enemyData["hp"]);
 
+
+// id="attack"をクリックしたらイベント発生
 document.getElementById("attack").addEventListener("click", function () {
-  enemyData["hp"] = enemyData["hp"] - 10;
+  let endGame = false;
+
+  // enemyData["hp"] = enemyData["hp"] - 10;↓
+  enemyData["hp"] -= 10;
+
+  // playerData["hp"] = playerData["hp"] - 10;↓
+  playerData["hp"] -= 10;
+
   insertText("currentEnemyHp", enemyData["hp"]);
+  insertText("currentPlayerHp", playerData["hp"]);
+
+  // 勝利条件
+  if (enemyData["hp"] <= 0) {
+    alert("勝利！");
+    endGame = true;
+
+  } else if(playerData["hp"] <= 0){
+    alert("敗北…");
+    endGame = true;
+
+  }
+
+  // endGame = true;ならば
+  if (endGame) {
+    // id="attack"をクリックしたら、新しいclass名を付ける
+    this.classList.add("deactive");
+  }
 });
